e960b9bf9894f8f4ce79df6c2272bd3f742c933d
Atamurad <atamurad.babakulyyev@gmail.com>
2021-08-11 15:01:49 -0400

    Create closure function which returns another function

--- app.ts
+++ app.ts
@@ -12,3 +12,16 @@ function makePlusFunction(baseNum: number): Ifunction {
 }
 const plus7 = makePlusFunction(7)
 plus7(5) //# output : 12
+
+//@------------------------------NEXT----------------------------@//
+// Closures are functions that remember their lexical environments. Lexical environments mean the environment in which the function was declared.
+
+function greetingMaker(str: string) {
+  function closure(name: string) {
+    return str + ',' + name
+  }
+  return closure
+}
+
+const greeting = greetingMaker('Hello')
+greeting('James') //➞ "Hello, James"
